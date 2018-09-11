00fd0c5aebf56fb2683dac7ff48050e0738b9a2b
paul <paul@pauls-MacBook-Pro.local>
2018-09-11 12:20:16 -0700

    basic layout setup complete

--- Yelp/BusinessCell.swift
+++ Yelp/BusinessCell.swift
@@ -21,12 +21,21 @@ class BusinessCell: UITableViewCell {
     var business: Business! {
         didSet{
             nameLabel.text = business.name
+            thumbImageView.setImageWith(business.imageURL!)
+            categoryLabel.text = business.categories
+            addressLabel.text = business.address
+            reviewCountLabel.text = "\(business.reviewCount!) Reviews"
+            ratingImageView.image = business.ratingImage
+            distanceLabel.text = business.distance
+            
         }
     }
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        thumbImageView.layer.cornerRadius = 5
+        thumbImageView.clipsToBounds = true
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
--- Yelp/BusinessesViewController.swift
+++ Yelp/BusinessesViewController.swift
@@ -20,6 +20,7 @@ class BusinessesViewController: UIViewController, UITableViewDelegate, UITableVi
         
         tableView.delegate = self
         tableView.dataSource = self
+        tableView.rowHeight = 150
         
         Business.searchWithTerm(term: "Thai", completion: { (businesses: [Business]?, error: Error?) -> Void in
             
