9d1ac889eb4dba728f77555a07baca9590c88168
paul <paul@sfs-wifi-dhcp-10-143-148-103.wifi.sfsu.edu>
2018-09-12 20:06:58 -0700

    layout constraint setup complete

--- Yelp/BusinessCell.swift
+++ Yelp/BusinessCell.swift
@@ -36,6 +36,9 @@ class BusinessCell: UITableViewCell {
         // Initialization code
         thumbImageView.layer.cornerRadius = 5
         thumbImageView.clipsToBounds = true
+        
+        //nameLabel.preferredMaxLayoutWidth = nameLabel.frame.size.width
+        
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
